28f8584d4486349bd4f39b5c53d0d758516e0261
rayyanaamir22 <92763211+rayyanaamir22@users.noreply.github.com>
2022-05-15 07:38:34 -0400

    Update variable names

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ Program: Hangman (Game Design Club Project)
 
 # Modules
 import os
-import functions
+import functions as f
 from images import HANGMANPICS
 
 missedLetters = ''
@@ -17,31 +17,31 @@ while True:
   # This part is like the main menu: The game starts here, and the player may return to this page to switch the word category.
   print('H A N G M A N')
   # Player selects category from which the random word is drawn.
-  functions.wordCategory()
+  f.wordCategory()
   # Game loop for chosen category begins.
   while True:
     os.system('clear')
-    functions.displayBoard(HANGMANPICS, missedLetters, correctLetters, functions.secretWord)
+    f.displayBoard(HANGMANPICS, missedLetters, correctLetters, f.secretWord)
 
     # Let the player type in a letter.
-    guess = functions.getGuess(missedLetters + correctLetters)
+    guess = f.getGuess(missedLetters + correctLetters)
 
-    if guess in functions.secretWord:
+    if guess in f.secretWord:
         # The program must count the non-letter characters as filled out spaces
         correctLetters = correctLetters + guess
         # Check if the player has won
         foundAllLetters = True
-        for i in range(len(functions.secretWord)):
+        for i in range(len(f.secretWord)):
             # Ignore non-letter characters when determining the amount of correct guesses
-            if functions.secretWord[i] not in functions.spaces and functions.secretWord[i] not in functions.dashes and functions.secretWord[i] not in functions.apostrophes and functions.secretWord[i] not in functions.colons:
-              if functions.secretWord[i] not in correctLetters:
+            if f.secretWord[i] not in f.spaces and f.secretWord[i] not in f.dashes and f.secretWord[i] not in f.apostrophes and f.secretWord[i] not in .colons:
+              if f.secretWord[i] not in correctLetters:
                 foundAllLetters = False
                 break
         if foundAllLetters:
             os.system('clear')
-            functions.winStreak += 1
-            functions.displayBoard(HANGMANPICS, missedLetters, correctLetters, functions.secretWord)
-            print('Yes! The secret word is "' + functions.secretWord +
+            f.winStreak += 1
+            f.displayBoard(HANGMANPICS, missedLetters, correctLetters, f.secretWord)
+            print('Yes! The secret word is "' + f.secretWord +
                   '"! You have won!')
             gameIsDone = True
     else:
@@ -49,14 +49,14 @@ while True:
         # Check if player has guessed too many times and lost
         if len(missedLetters) == len(HANGMANPICS) - 1:
             os.system('clear')
-            functions.winStreak = 0
-            functions.displayBoard(HANGMANPICS, missedLetters, correctLetters,functions.secretWord)
+            f.winStreak = 0
+            f.displayBoard(HANGMANPICS, missedLetters, correctLetters,f.secretWord)
             # Inform user they have lost, and present the amount of correct and incorrect guesses
             # If the amount of correct guesses is 1, say "1 correct guess" instead of "1 correct guesses"
             if int(len(correctLetters)) != 1:
-              print('You have run out of guesses!\nAfter ' + str(len(missedLetters)) + ' missed guesses and ' + str(len(correctLetters)) + ' correct guesses, the word was "' + functions.secretWord + '"')
+              print('You have run out of guesses!\nAfter ' + str(len(missedLetters)) + ' missed guesses and ' + str(len(correctLetters)) + ' correct guesses, the word was "' + f.secretWord + '"')
             elif int(len(correctLetters)) == 1:
-              print('You have run out of guesses!\nAfter ' + str(len(missedLetters)) + ' missed guesses and ' + str(len(correctLetters)) + ' correct guess, the word was "' + functions.secretWord + '"')
+              print('You have run out of guesses!\nAfter ' + str(len(missedLetters)) + ' missed guesses and ' + str(len(correctLetters)) + ' correct guess, the word was "' + f.secretWord + '"')
             else:
               continue
 
@@ -64,17 +64,17 @@ while True:
 
     # Ask the player if they want to play again (but only if the game is done).
     if gameIsDone:
-        if functions.playAgain():
+        if f.playAgain():
             missedLetters = ''
             correctLetters = ''
             gameIsDone = False
             # If the player wants to do the same category again, select another random word from the current category
-            functions.nextSecretWord()
+            f.nextSecretWord()
         else: 
           break
 
   # Ask the player if they want to return to main menu
-  if not functions.changeCategory():
+  if not f.changeCategory():
     # If the user wants to exit the entire program, the loop breaks here
     os.system('clear')
     print('Thanks for playing!')
@@ -91,3 +91,9 @@ while True:
     continue
 
 # End of game
+
+
+'''
+IDEAS:
+- Allow the user to create their own category and save it using SQL
+'''
